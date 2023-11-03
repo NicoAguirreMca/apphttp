ceea9f2857e3fe040e166330d13466c3ece46f51
NicoAguirreMca <naguirre@mcanet.com.ar>
2023-11-03 11:53:18 -0300

    add respuesta cars y tours

--- src/providers/http/http.service.ts
+++ src/providers/http/http.service.ts
@@ -22,18 +22,18 @@ export class HttpCustomService {
       const headersRequest = {
         'Content-Type': 'application/x-www-form-urlencoded', // afaik this one is not needed
       };
-      console.log(process.env.CLIENT_ID)
-      console.log(process.env.CLIENT_SECRET)
-      const data = {
+      //console.log(process.env.CLIENT_ID)
+      //console.log(process.env.CLIENT_SECRET)
+      /*const data = {
         client_id:process.env.CLIENT_ID ,
         client_secret:process.env.CLIENT_SECRET,
         grant_type:'client_credentials'
-      };
-      /*const data = {
+      };*/
+      const data = {
         client_id:'vz2KuAJyCNwXy03bygG9ddCcyD0ApMnc ',
         client_secret:'fvQlVTeYMG3DAUiC',
         grant_type:'client_credentials'
-      };*/
+      };
 
         const checkResultObservable= this.httpService.post("https://test.api.amadeus.com/v1/security/oauth2/token",data,{headers: headersRequest })
         const checkResult = await (await lastValueFrom(checkResultObservable)).data;
@@ -102,8 +102,9 @@ export class HttpCustomService {
         
         try {
            	//DOCS DE API
-		//https://developers.amadeus.com/self-service/category/destination-experiences/api-doc/tours-and-activities/api-reference
-            const checkResultObservable= this.httpService.get("https://test.api.amadeus.com/v1/shopping/flight-destinations?origin=PAR&maxPrice=200",{headers: headersRequest })
+		        //https://developers.amadeus.com/self-service/category/destination-experiences/api-doc/tours-and-activities/api-reference
+            // ver variables latitude=41.397158&longitude=2.160873&radius=1
+            const checkResultObservable= this.httpService.get("https://test.api.amadeus.com/v1/shopping/activities?latitude=41.397158&longitude=2.160873&radius=1",{headers: headersRequest })
             //console.log(checkResultObservable);
             const checkResult = await (await lastValueFrom(checkResultObservable)).data;
             console.log(checkResult);
@@ -121,11 +122,60 @@ export class HttpCustomService {
         const headersRequest = {
           Authorization: `Bearer ${token}`
         };
-        
+
+        const data ={
+          "startLocationCode": "CDG",
+          "endAddressLine": "Avenue Anatole France, 5",
+          "endCityName": "Paris",
+          "endZipCode": "75007",
+          "endCountryCode": "FR",
+          "endName": "Souvenirs De La Tour",
+          "endGeoCode": "48.859466,2.2976965",
+          "transferType": "PRIVATE",
+          "startDateTime": "2023-11-10T10:30:00",
+          "providerCodes": "TXO",
+          "passengers": 2,
+          "stopOvers": [
+            {
+              "duration": "PT2H30M",
+              "sequenceNumber": 1,
+              "addressLine": "Avenue de la Bourdonnais, 19",
+              "countryCode": "FR",
+              "cityName": "Paris",
+              "zipCode": "75007",
+              "name": "De La Tours",
+              "geoCode": "48.859477,2.2976985",
+              "stateCode": "FR"
+            }
+          ],
+          "startConnectedSegment": {
+            "transportationType": "FLIGHT",
+            "transportationNumber": "AF380",
+            "departure": {
+              "localDateTime": "2023-11-10T09:00:00",
+              "iataCode": "NCE"
+            },
+            "arrival": {
+              "localDateTime": "2023-11-10T10:00:00",
+              "iataCode": "CDG"
+            }
+          },
+          "passengerCharacteristics": [
+            {
+              "passengerTypeCode": "ADT",
+              "age": 20
+            },
+            {
+              "passengerTypeCode": "CHD",
+              "age": 10
+            }
+          ]
+          };
+       	
         try {
-		//DOCS API
-		//https://developers.amadeus.com/self-service/category/cars-and-transfers/api-doc/transfer-search/api-reference
-            const checkResultObservable= this.httpService.get("https://test.api.amadeus.com/v1/shopping/flight-destinations?origin=PAR&maxPrice=200",{headers: headersRequest })
+              //DOCS API
+		          //https://developers.amadeus.com/self-service/category/cars-and-transfers/api-doc/transfer-search/api-reference
+            const checkResultObservable= this.httpService.post("https://test.api.amadeus.com/v1/shopping/transfer-offers",data,{headers: headersRequest })
             //console.log(checkResultObservable);
             const checkResult = await (await lastValueFrom(checkResultObservable)).data;
             console.log(checkResult);
